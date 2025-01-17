37d29a0202ccfa355777b1ebefbc282bce56ac63
schooldev49 <102310270+schooldev49@users.noreply.github.com>
2025-01-17 10:02:05 -0500

    Create MyProgram.java

---
+++ MyProgram.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class MyProgram
+{
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("LOADING CUBE...");
+        
+        Cube cube = InputParser.parseInput("assets/beesetup2.txt");
+        
+        
+        
+        
+    }
+}
